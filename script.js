4c20b8be9e4793ddbcccb82ad12413edfa9987b3
IbrahimMurad <Ibrahimmorad31@gmail.com>
2025-02-16 03:31:04 +0200

    fixing some accessibility issues

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const accordions = document.querySelectorAll('.accordion');
 
 
 accordions.forEach(accordion => {
-    const question = accordion.querySelector('.accordion__question');
+    const question = accordion.querySelector('.question');
     const answer = accordion.querySelector('.accordion__answer');
     const button = accordion.querySelector('button');
 
-    question.addEventListener('click', () => {
+    const toggleAccordion = () => {
         const expanded = question.getAttribute('aria-expanded') === 'true' || false;
 
         question.setAttribute('aria-expanded', !expanded);
+        question.classList.toggle('open');
         button.setAttribute('aria-expanded', !expanded);
         answer.hidden = expanded;
-    });
+    };
+
+    question.addEventListener('click', toggleAccordion);
+    button.addEventListener('click', toggleAccordion);
 });
